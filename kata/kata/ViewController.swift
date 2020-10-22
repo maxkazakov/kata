a92ce8f3e7b0be397971b86fd86015e598b98da9
Maxim Kazakov <kazakov.maxim.a@icloud.com>
2020-10-22 13:43:57 +0500

    init

---
+++ kata/kata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kata
+//
+//  Created by Максим Казаков on 22.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
